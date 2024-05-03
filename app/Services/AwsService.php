30f0f2bfae8d377ca4e367a8715db13a718ea5de
temirlankassym <kassym22t@gmail.com>
2024-05-03 21:43:53 +0500

    unique name to files

--- app/Services/AwsService.php
+++ app/Services/AwsService.php
@@ -3,12 +3,14 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class AwsService{
     public function store($file)
     {
         if($file){
-            $path = $file->getClientOriginalName();
+            $name = $file->getClientOriginalName();
+            $path = $name.''.Str::random(10);
             Storage::disk('s3')->put($path,file_get_contents($file),'public');
             $link = Storage::disk('s3')->url($path);
             return $link;
