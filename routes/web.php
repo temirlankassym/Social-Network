488fe35dd87f5bdd2352a810396184c3a9d7af4b
temirlankassym <kassym22t@gmail.com>
2024-05-04 14:49:18 +0500

    Добавил websockets

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/messages', [HomeController::class, 'messages'])->name('messages');
+
+Route::post('/message', [HomeController::class, 'message'])->name('message');
