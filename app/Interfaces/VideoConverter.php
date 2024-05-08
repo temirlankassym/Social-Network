55732a01461ede495fc8f0a8e8812331533bc9a0
temirlankassym <kassym22t@gmail.com>
2024-05-09 00:46:01 +0500

    interface

--- app/Interfaces/VideoConverter.php
+++ app/Interfaces/VideoConverter.php
@@ -5,4 +5,6 @@ namespace App\Interfaces;
 interface VideoConverter
 {
     public function convertVideo($file);
+    public function resolveExtension($content);
+    public function delete($content);
 }
